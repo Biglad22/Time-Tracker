67ad7afd636793fe58db9d5fab849b816f2d0119
Biglad22 <emmanuelaberuagba22@gmail.com>
2024-03-13 06:47:57 +0100

    2:30 fe lu

--- src/components/tasktag.vue
+++ src/components/tasktag.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="tag flex flex-wrap flex-row gap-1.5 items-center ps-0 sm:ps-3.5 p-3.5 opacity-80">
         <tasktag class=" bg-transparent" :tagColor="tagColor" />
-        <p class="tag-name font-normal uppercase text-base opacity-80" :style="{color: tagColor !== null ? `${tagColor}` : 'var(--text-m)'}">
+        <small class="tag-name font-normal uppercase text-base opacity-80" :style="{color: tagColor !== null ? `${tagColor}` : 'var(--text-m)'}">
             <slot name="tag"></slot>
-        </p>
+        </small>
     </div>
 </template>
 <script>
